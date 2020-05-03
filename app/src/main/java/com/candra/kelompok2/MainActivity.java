682e57eae8ba20c2c0207e3a73bb700e8b19cb04
candraseng <candrabillysagita@gmail.com>
2020-05-03 10:54:23 +0700

    Update

--- app/src/main/java/com/candra/kelompok2/MainActivity.java
+++ app/src/main/java/com/candra/kelompok2/MainActivity.java
@@ -3,12 +3,54 @@ package com.candra.kelompok2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class MainActivity extends AppCompatActivity {
+    //    public static final String URL = "http://10.10.23.142:8000/";
+//    public static final String URL = "http://192.168.43.102:8000/";
+    public static final String URL = "http://10.10.22.128:8000/";
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navigation_user:
+                    selectedFragment = new UserFragment();
+                    break;
+                case R.id.navigation_comment:
+                    selectedFragment = new CommentFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("Tokensaya", FirebaseInstanceId.getInstance().getToken());
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
 }
--- app/src/main/java/com/candra/kelompok2/activity/LoginActivity.java
+++ app/src/main/java/com/candra/kelompok2/activity/LoginActivity.java
@@ -21,6 +21,7 @@ import android.net.Uri;
 import android.view.View;
 import android.content.SharedPreferences;
 
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -40,41 +41,61 @@ import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity{
-    EditText username, password;
-    Button btnLogin;
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mloginBtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Membuat FullScreen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mEmailField = (EditText) findViewById(R.id.etEmail);
+        mPasswordField = (EditText) findViewById(R.id.etPassword);
+        mloginBtn = (Button) findViewById(R.id.btnLogin);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
 
-        username = (EditText) findViewById(R.id.etUsername);
-        password = (EditText) findViewById(R.id.etPassword);
-        btnLogin = (Button)findViewById(R.id.btnLogin);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                String usernameKey = username.getText().toString();
-                String passwordKey = password.getText().toString();
-
-                if (usernameKey.equals("admin") && passwordKey.equals("1234")){
-                    //jika login berhasil
-                    Toast.makeText(getApplicationContext(), "LOGIN SUKSES",
-                            Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(LoginActivity.this, BerandaActivity.class);
-                    LoginActivity.this.startActivity(intent);
-                    finish();
-                }else {
-                    //jika login gagal
-                    AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                    builder.setMessage("Username atau Password Anda salah!")
-                            .setNegativeButton("Retry", null).create().show();
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class) );
                 }
             }
-
+        };
+        mloginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSignIn();
+            }
         });
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    private void startSignIn() {
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+        if (TextUtils.isEmpty(email) | TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "Fields are Empty", Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Login Problem", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+        }
+
+    }
+
 }
